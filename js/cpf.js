c264186b577bbe262c5ca15197e78327c620b15d
victor <joao.victormeneses@ufrpe.br>
2023-10-01 15:54:23 -0300

    validação de cpf

---
+++ js/cpf.js
@@ -0,0 +1,13 @@
+
+
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    console.log(cpf);
+}
+
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    validaNumerosRepetidos(cpf);
+
+    console.log(validaNumerosRepetidos(cpf));
+}
